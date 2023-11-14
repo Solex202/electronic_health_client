a0971b4c082851d38e3caf3b1e3289cbef1b528b
lota <christiansolomon592@gmail.com>
2023-11-14 12:25:05 +0100

    creating registration page

--- src/pages/auth/Register/index.jsx
+++ src/pages/auth/Register/index.jsx
@@ -6,13 +6,15 @@ const Register
         return (
             <div className={style.parent}>
                 <div id='left-section' className={style.left_section}>
-                    <div  className={style.logo_div}>
-                        <img id='logo' className={style.logo} src={logo}/>
-                        <p>LotaMedics</p>
-                    </div>
-                    <div className={style.intro_text}>
-                        <h2 >"Your Wellbeing, <br/>Your Way."</h2>
-                        <p >Your Wellbeing & healthcare made easy and tailored to your taste.</p>
+                    <div className={style.left_section_inner}>
+                        <div  className={style.logo_div}>
+                            <img id='logo' className={style.logo} src={logo}/>
+                            <h3>LotaMedics</h3>
+                        </div>
+                        <div className={style.intro_text}>
+                            <p className={style.intro_text_p1}>"Your Wellbeing, <br/>Your Way."</p>
+                            <p className={style.intro_text_p2}>Your Wellbeing & healthcare<br/> made easy and tailored to <br/> your taste.</p>
+                        </div>
                     </div>
                 </div>
                 <div>dev</div>
