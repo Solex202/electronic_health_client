bfcf389c821af3166278cfd58c0ba31dbca02775
lota <christiansolomon592@gmail.com>
2023-11-14 11:35:14 +0100

    creating registration page

--- src/pages/auth/Register/index.jsx
+++ src/pages/auth/Register/index.jsx
@@ -4,15 +4,15 @@ import logo from "../../../assets/LotaMedics Logo.svg"
 const Register
     = () => {
         return (
-            <div id='parent'>
+            <div className={style.parent}>
                 <div id='left-section' className={style.left_section}>
                     <div  className={style.logo_div}>
                         <img id='logo' className={style.logo} src={logo}/>
                         <p>LotaMedics</p>
                     </div>
-                    <div>
-                        <p>Your Wellbeing, Your Way.</p>
-                        <p>Your Wellbeing & healthcare made easy and tailored to your taste.</p>
+                    <div className={style.intro_text}>
+                        <h2 >"Your Wellbeing, <br/>Your Way."</h2>
+                        <p >Your Wellbeing & healthcare made easy and tailored to your taste.</p>
                     </div>
                 </div>
                 <div>dev</div>
